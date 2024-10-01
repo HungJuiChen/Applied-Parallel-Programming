58a4e937baf64a3d04926d86149eb286795755ff
HungJuiChen <hungjui2@illinois.edu>
2024-09-30 20:36:11 -0500

    Update code

--- lab4/lab4.cu
+++ lab4/lab4.cu
@@ -11,12 +11,48 @@
   } while (0)
 
 //@@ Define any useful program-wide constants here
+#define BLOCK_SIZE 8  // Define the size of CUDA blocks (8x8x8)
 
 //@@ Define constant memory for device kernel here
+__constant__ float d_Kernel[27];  // 3x3x3 convolution kernel stored in constant memory
 
 __global__ void conv3d(float *input, float *output, const int z_size,
                        const int y_size, const int x_size) {
   //@@ Insert kernel code here
+  // Calculate the 3D index of the current thread
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int z = blockIdx.z * blockDim.z + threadIdx.z;
+
+  // Calculate the linear index for the output
+  int output_index = z * y_size * x_size + y * x_size + x;
+
+  if (x < x_size && y < y_size && z < z_size) {
+    float sum = 0.0f;
+
+    // Iterate over the 3x3x3 kernel
+    for (int kz = -1; kz <= 1; kz++) {
+      for (int ky = -1; ky <= 1; ky++) {
+        for (int kx = -1; kx <= 1; kx++) {
+          int in_z = z + kz;
+          int in_y = y + ky;
+          int in_x = x + kx;
+
+          // Check for boundary conditions
+          if (in_z >= 0 && in_z < z_size &&
+              in_y >= 0 && in_y < y_size &&
+              in_x >= 0 && in_x < x_size) {
+            int input_index = in_z * y_size * x_size + in_y * x_size + in_x;
+            float input_val = input[input_index];
+            float kernel_val = d_Kernel[(kz + 1) * 9 + (ky + 1) * 3 + (kx + 1)];
+            sum += input_val * kernel_val;
+          }
+        }
+      }
+    }
+
+    output[output_index] = sum;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -29,6 +65,8 @@ int main(int argc, char *argv[]) {
   float *hostKernel;
   float *hostOutput;
   //@@ Initial deviceInput and deviceOutput here.
+  float *deviceInput;
+  float *deviceOutput;
 
   args = wbArg_read(argc, argv);
 
@@ -50,20 +88,31 @@ int main(int argc, char *argv[]) {
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
   // because the first  three elements were the dimensions
-
+  wbCheck(cudaMalloc((void **)&deviceInput, (inputLength - 3) * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&deviceOutput, (inputLength - 3) * sizeof(float)));
 
 
   //@@ Copy input and kernel to GPU here
   // Recall that the first three elements of hostInput are dimensions and
   // do
   // not need to be copied to the gpu
-
+  wbCheck(cudaMemcpy(deviceInput, hostInput + 3, (inputLength - 3) * sizeof(float), cudaMemcpyHostToDevice));
+  // Copy the kernel to constant memory
+  wbCheck(cudaMemcpyToSymbol(d_Kernel, hostKernel, kernelLength * sizeof(float)));
 
 
   //@@ Initialize grid and block dimensions here
-
+  dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gridDim(
+      (x_size + blockDim.x - 1) / blockDim.x,
+      (y_size + blockDim.y - 1) / blockDim.y,
+      (z_size + blockDim.z - 1) / blockDim.z
+  );
   //@@ Launch the GPU kernel here
+  conv3d<<<gridDim, blockDim>>>(deviceInput, deviceOutput, z_size, y_size, x_size);
+  wbCheck(cudaGetLastError());
 
+  // Wait for the GPU to finish
   cudaDeviceSynchronize();
 
 
@@ -71,7 +120,7 @@ int main(int argc, char *argv[]) {
   //@@ Copy the device memory back to the host here
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
-
+  wbCheck(cudaMemcpy(hostOutput + 3, deviceOutput, (inputLength - 3) * sizeof(float), cudaMemcpyDeviceToHost));
 
 
 
@@ -82,7 +131,8 @@ int main(int argc, char *argv[]) {
   wbSolution(args, hostOutput, inputLength);
 
   //@@ Free device memory
-
+  wbCheck(cudaFree(deviceInput));
+  wbCheck(cudaFree(deviceOutput));
 
   // Free host memory
   free(hostInput);
