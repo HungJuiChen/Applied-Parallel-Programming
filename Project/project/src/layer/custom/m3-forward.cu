44fb9c1e97d27e78ddc0a43187e66bcfc0234846
HungJuiChen <hungjui2@illinois.edu>
2024-12-04 14:18:43 -0600

    op_2 loop unrolling

--- Project/project/src/layer/custom/m3-forward.cu
+++ Project/project/src/layer/custom/m3-forward.cu
@@ -6,7 +6,7 @@
 #define BLOCK_SIZE 256
 #define MAX_BATCH_SIZE 1000
 
-__global__ void matrix_unrolling_kernel(const float *__restrict__ input, float *__restrict__ output,
+__global__ void matrix_unrolling_kernel(const float *input, float *output,
                                         const int Batch, const int Channel,
                                         const int Height, const int Width,
                                         const int K) {
@@ -24,12 +24,12 @@ __global__ void matrix_unrolling_kernel(const float *__restrict__ input, float *
     */
     const int Height_out = Height - K + 1;
     const int Width_out = Width - K + 1;
-    
+
     const int H_unroll = Channel * K * K;
     const int W_unroll = Batch * Height_out * Width_out;
 
     // Calculate h_unroll and w_unroll using 2D grid and block indices
-    int h_unroll = blockIdx.y * blockDim.y + threadIdx.y;
+    //int h_unroll = blockIdx.y * blockDim.y + threadIdx.y;
     int w_unroll = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (h_unroll < H_unroll && w_unroll < W_unroll) {
@@ -50,24 +50,31 @@ __global__ void matrix_unrolling_kernel(const float *__restrict__ input, float *
         // An example use of these macros:
         // float a = in_4d(0,0,0,0)
 
-        #define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
-        #define out_2d(i1, i0) output[(i1) * W_unroll + (i0)]
+        // Unroll over h_unroll
+        #pragma unroll
+        for (int c = 0; c < Channel; ++c) {
+            for (int p = 0; p < K; ++p) {
+                for (int q = 0; q < K; ++q) {
+                    int h_unroll = c * K * K + p * K + q;
+                    int input_row = h + p;
+                    int input_col = w + q;
 
-        // TODO: Insert your input matrix unrolling kernel code here
-        if (input_row < Height && input_col < Width) {
-            out_2d(h_unroll, w_unroll) = in_4d(b, c, input_row, input_col);
-        } else {
-            out_2d(h_unroll, w_unroll) = 0.0f;
+                    float val = 0.0f;
+                    if (input_row < Height && input_col < Width) {
+                        val = input[b * Channel * Height * Width +
+                                    c * Height * Width +
+                                    input_row * Width + input_col];
+                    }
+                    output[h_unroll * W_unroll + w_unroll] = val;
+                }
+            }
         }
-
-        #undef in_4d
-        #undef out_2d
     }
 }
 
 // Tiled matrix multiplication kernel. Computes C = AB
 // You don't need to modify this kernel.
-__global__ void matrixMultiplyShared(const float *__restrict__ A, const float *__restrict__ B, float *__restrict__ C,
+__global__ void matrixMultiplyShared(const float *A, const float *B, float *C,
                                      int numARows, int numAColumns,
                                      int numBRows, int numBColumns,
                                      int numCRows, int numCColumns)
@@ -94,9 +101,11 @@ __global__ void matrixMultiplyShared(const float *__restrict__ A, const float *_
         __syncthreads();
 
         if (row < numCRows && col < numCColumns) {
+            // Unrolled loop
+            #pragma unroll
             for (int i = 0; i < TILE_WIDTH; i++) {
                 val += tileA[ty][i] * tileB[i][tx];
-            }
+            }  
         }
         __syncthreads();
     }
@@ -110,11 +119,13 @@ __global__ void matrixMultiplyShared(const float *__restrict__ A, const float *_
 // The output feature map after matmul is of shape Map_out x Batch x Height_out x Width_out,
 // and we need to permute it into Batch x Map_out x Height_out x Width_out.
 // You don't need to modify this kernel.
-__global__ void matrix_permute_kernel(const float *__restrict__ input, float *__restrict__ output, int Map_out,
+__global__ void matrix_permute_kernel(const float *input, float *output, int Map_out,
                                       int Batch, int image_size) {
     int b = blockIdx.y;
     int x = blockIdx.x * BLOCK_SIZE + threadIdx.x;
     if (x < image_size) {
+        // Unroll over Map_out if it's small
+        #pragma unroll
         for (int m = 0; m < Map_out; m++) {
             output[b * Map_out * image_size + m * image_size + x] =
                     input[m * Batch * image_size + b * image_size + x];
@@ -125,17 +136,6 @@ __global__ void matrix_permute_kernel(const float *__restrict__ input, float *__
 __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, const float *host_input, const float *host_mask, float **device_output_ptr, float **device_input_ptr, float **device_mask_ptr, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
 {
     // TODO: Allocate memory and copy over the relevant data structures to the GPU
-
-    // We pass double pointers for you to initialize the relevant device pointers,
-    //  which are passed to the other two functions.
-
-    // Useful snippet for error checking
-    // cudaError_t error = cudaGetLastError();
-    // if(error != cudaSuccess)
-    // {
-    //     std::cout<<"CUDA error: "<<cudaGetErrorString(error)<<std::endl;
-    //     exit(-1);
-    // }
 
     // Allocate device memory for input
     size_t input_size = Batch * Channel * Height * Width * sizeof(float);
@@ -181,7 +181,6 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
     cudaMalloc((void**)&unrolled_matrix, max_unroll_size);
 
     size_t max_matmul_size = Map_out * (MAX_BATCH_SIZE * Height_out * Width_out) * sizeof(float);
-    
     cudaMalloc((void**)&matmul_output, max_matmul_size);
     // TODO: Set the kernel dimensions and call the matrix unrolling kernel.
     
